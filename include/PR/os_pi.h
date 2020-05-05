cb46c7693e30c1be41a2948a5cbee1da21c4a6ef
Felipe Guaycuru de C. B. Franco <guaycuru@gmail.com>
2020-05-05 17:29:50 -0300

    Fix conflicting definitions for `osEPiRawStartDma()`

--- include/PR/os_pi.h
+++ include/PR/os_pi.h
@@ -83,5 +83,5 @@ s32 osPiWriteIo(uintptr_t devAddr, u32 data);
 s32 osPiReadIo(uintptr_t devAddr, u32 *data);
 
 s32 osPiRawStartDma(s32 dir, u32 cart_addr, void *dram_addr, size_t size);
-s32 osEPiRawStartDma(OSPiHandle *piHandle, s32 dir, u32 cart_addr, void *dram_addr, size_t size);
+s32 osEPiRawStartDma(OSPiHandle *piHandle, s32 dir, u32 cart_addr, void *dram_addr, u32 size);
 #endif
